8f6136afc9eb82ae62b099dce20be59c79fa1a42
zhongjiaqi <920093991@qq.com>
2023-05-31 21:04:10 +0800

    Go指南学习-concurrency-完成web爬虫练习

--- src/concurrency/10_exercise-web-crawler.go
+++ src/concurrency/10_exercise-web-crawler.go
@@ -9,24 +9,41 @@ import (
 // 修改 Crawl 函数来并行地抓取 URL，并且保证不重复。
 // 提示：你可以用一个 map 来缓存已经获取的 URL，但是要注意 map 本身并不是并发安全的！
 
-type SafeMap struct {
-	v   map[string]int
-	mux sync.Mutex // 访问互斥锁
-}
-
 type Fetcher interface {
 	// Fetch 返回 URL 的 body 内容，并且将在这个页面上找到的 URL 放到一个 slice 中。
 	Fetch(url string) (body string, urls []string, err error)
 }
 
+type SafeMap struct {
+	v   map[string]int
+	mux sync.Mutex     // 访问互斥锁
+	wg  sync.WaitGroup // 等待组
+}
+
+var c = SafeMap{v: make(map[string]int)}
+
+func (c *SafeMap) exist(url string) bool {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+
+	_, ok := c.v[url] // 判断url是否存在
+	if ok {
+		return true
+	} else {
+		c.v[url] = 1
+		c.wg.Add(1)
+		return false
+	}
+}
+
 // Crawl 使用 fetcher 从某个 URL 开始递归的爬取页面，直到达到最大深度。
 func Crawl(url string, depth int, fetcher Fetcher) {
-	// TODO: 并行的抓取 URL。
-	// TODO: 不重复抓取页面。
-	// 下面并没有实现上面两种情况：
+	defer c.wg.Done()
+
 	if depth <= 0 {
 		return
 	}
+
 	body, urls, err := fetcher.Fetch(url)
 	if err != nil {
 		fmt.Println(err)
@@ -34,13 +51,18 @@ func Crawl(url string, depth int, fetcher Fetcher) {
 	}
 	fmt.Printf("found: %s %q\n", url, body)
 	for _, u := range urls {
-		Crawl(u, depth-1, fetcher)
+		if c.exist(u) == false { // 如果url不存在才递归
+			go Crawl(u, depth-1, fetcher) // 并发
+		}
 	}
 	return
 }
 
 func main() {
-	Crawl("https://golang.org/", 4, fetcher)
+	url := "https://golang.org/"
+	c.exist(url)
+	Crawl(url, 4, fetcher)
+	c.wg.Wait()
 }
 
 // fakeFetcher 是返回若干结果的 Fetcher。
