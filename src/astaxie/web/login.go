64d78a0b547bbd08626bb08690df911e3f83ca36
zhongjiaqi <920093991@qq.com>
2023-06-05 21:00:12 +0800

    修改昨天的错误 + 数据库第一章

--- src/astaxie/web/login.go
+++ src/astaxie/web/login.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func sayhelloName1(w http.ResponseWriter, r *http.Request) {
@@ -26,17 +31,49 @@ func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method) //获取请求的方法
 	if r.Method == "GET" {
 		t, _ := template.ParseFiles("login.gtpl")
-		log.Println(t.Execute(w, nil)) // 运行报错
+		log.Println(t.Execute(w, nil))
 	} else {
 		//请求的是登录数据，那么执行登录的逻辑判断
+		r.ParseForm()
 		fmt.Println("username:", r.Form["username"])
 		fmt.Println("password:", r.Form["password"])
 	}
 }
 
+// 处理/upload 逻辑
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //获取请求的方法
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("upload.gtpl")
+		t.Execute(w, token)
+	} else {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666) // 此处假设当前目录下已存在test目录
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", sayhelloName1)      //设置访问的路由
-	http.HandleFunc("/login", login)         //设置访问的路由
+	http.HandleFunc("/", sayhelloName1) //设置访问的路由
+	http.HandleFunc("/login", login)    //设置访问的路由
+	http.HandleFunc("/upload", upload)
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
