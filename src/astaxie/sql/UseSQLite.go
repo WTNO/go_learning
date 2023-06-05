b4a2a2876a424be1124a2cd4c261af21ce78a6ed
zhongjiaqi <920093991@qq.com>
2023-06-05 23:39:40 +0800

    解决了go get引包失败的问题 学习了SQLite等数据库的驱动使用 学习了go语言的进行ORM操作的库beego的使用

--- src/astaxie/sql/UseSQLite.go
+++ src/astaxie/sql/UseSQLite.go
@@ -3,17 +3,17 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	//"time"
+	"time"
 
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root:123456@/test?charset=utf8")
+	db, err := sql.Open("sqlite3", "./foo.db")
 	checkErr(err)
 
 	//插入数据
-	stmt, err := db.Prepare("INSERT INTO userinfo SET username=?,department=?,created=?")
+	stmt, err := db.Prepare("INSERT INTO userinfo(username, department, created) values(?,?,?)")
 	checkErr(err)
 
 	res, err := stmt.Exec("astaxie", "研发部门", "2012-12-09")
@@ -43,7 +43,7 @@ func main() {
 		var uid int
 		var username string
 		var department string
-		var created string
+		var created time.Time
 		err = rows.Scan(&uid, &username, &department, &created)
 		checkErr(err)
 		fmt.Println(uid)
@@ -54,13 +54,13 @@ func main() {
 
 	//删除数据
 	stmt, err = db.Prepare("delete from userinfo where uid=?")
-	checkErr(err)
+	checkErr1(err)
 
 	res, err = stmt.Exec(id)
-	checkErr(err)
+	checkErr1(err)
 
 	affect, err = res.RowsAffected()
-	checkErr(err)
+	checkErr1(err)
 
 	fmt.Println(affect)
 
@@ -68,7 +68,7 @@ func main() {
 
 }
 
-func checkErr(err error) {
+func checkErr1(err error) {
 	if err != nil {
 		panic(err)
 	}
